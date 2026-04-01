6acb799ab450d7a50b1babc62597e2a319a60d36
Anthony Libetti <tooch@Mac.hsd1.pa.comcast.net>
2026-03-31 23:21:06 -0400

    Pull new generated types

--- src/lib/supabase/database.types.ts
+++ src/lib/supabase/database.types.ts
@@ -1,78 +1,220 @@
-export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[];
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
 
 export type Database = {
-	public: {
-		Tables: {
-			musings: {
-				Row: {
-					id: string;
-					author_id: string;
-					author_label: string;
-					title: string | null;
-					body: string;
-					created_at: string;
-				};
-				Insert: {
-					id?: string;
-					author_id: string;
-					author_label: string;
-					title?: string | null;
-					body: string;
-					created_at?: string;
-				};
-				Update: {
-					id?: string;
-					author_id?: string;
-					author_label?: string;
-					title?: string | null;
-					body?: string;
-					created_at?: string;
-				};
-				Relationships: [];
-			};
-			profiles: {
-				Row: {
-					id: string;
-					username: string;
-					username_normalized: string;
-					first_name: string;
-					last_name: string;
-					country: string;
-					state: string;
-					city: string;
-					created_at: string;
-					updated_at: string;
-				};
-				Insert: {
-					id: string;
-					username: string;
-					username_normalized: string;
-					first_name: string;
-					last_name: string;
-					country: string;
-					state: string;
-					city: string;
-					created_at?: string;
-					updated_at?: string;
-				};
-				Update: {
-					id?: string;
-					username?: string;
-					username_normalized?: string;
-					first_name?: string;
-					last_name?: string;
-					country?: string;
-					state?: string;
-					city?: string;
-					created_at?: string;
-					updated_at?: string;
-				};
-				Relationships: [];
-			};
-		};
-		Views: Record<string, never>;
-		Functions: Record<string, never>;
-		Enums: Record<string, never>;
-		CompositeTypes: Record<string, never>;
-	};
-};
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "14.1"
+  }
+  public: {
+    Tables: {
+      musings: {
+        Row: {
+          author_id: string
+          author_label: string
+          body: string
+          created_at: string
+          id: string
+          title: string | null
+        }
+        Insert: {
+          author_id: string
+          author_label: string
+          body: string
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+        Update: {
+          author_id?: string
+          author_label?: string
+          body?: string
+          created_at?: string
+          id?: string
+          title?: string | null
+        }
+        Relationships: []
+      }
+      profiles: {
+        Row: {
+          city: string
+          country: string
+          created_at: string
+          first_name: string
+          id: string
+          last_name: string
+          state: string
+          updated_at: string
+          username: string
+          username_normalized: string
+        }
+        Insert: {
+          city: string
+          country: string
+          created_at?: string
+          first_name: string
+          id: string
+          last_name: string
+          state: string
+          updated_at?: string
+          username: string
+          username_normalized: string
+        }
+        Update: {
+          city?: string
+          country?: string
+          created_at?: string
+          first_name?: string
+          id?: string
+          last_name?: string
+          state?: string
+          updated_at?: string
+          username?: string
+          username_normalized?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
+
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof DatabaseWithoutInternals },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof DatabaseWithoutInternals },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof DatabaseWithoutInternals },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof DatabaseWithoutInternals
+  }
+    ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends {
+  schema: keyof DatabaseWithoutInternals
+}
+  ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {},
+  },
+} as const
